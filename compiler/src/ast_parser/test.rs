5a288e8f73f351e8d91c5180cbaef0755c8abef4
Nhat Tien <ledangnhattien04@gmail.com>
2025-08-26 21:41:39 +0700

    test: add more test

--- compiler/src/ast_parser/test.rs
+++ compiler/src/ast_parser/test.rs
@@ -51,3 +51,64 @@ fn ast_test_4() {
     });
 }
 
+#[test]
+fn ast_test_5() {
+    let mut lex = Lexer::new_from_str("1 + 2 * 5\n");
+    let expr = parse(&mut lex);
+    assert_eq!(expr, Expr::Binary {
+        op: Operator::Add,
+        lhs: Box::new(Expr::Number(1)),
+        rhs: Box::new(Expr::Binary {
+            op: Operator::Multi,
+            lhs: Box::new(Expr::Number(2)),
+            rhs: Box::new(Expr::Number(5)),
+        }),
+    });
+}
+
+
+#[test]
+fn ast_test_6() {
+    let mut lex = Lexer::new_from_str("1 * 2 + 5\n");
+    let expr = parse(&mut lex);
+    assert_eq!(expr, Expr::Binary {
+        op: Operator::Add,
+        lhs: Box::new(Expr::Binary {
+            op: Operator::Multi,
+            lhs: Box::new(Expr::Number(1)),
+            rhs: Box::new(Expr::Number(2)),
+        }),
+        rhs: Box::new(Expr::Number(5)),
+    });
+}
+
+#[test]
+fn ast_test_7() {
+    let mut lex = Lexer::new_from_str("1 + 2 + 5\n");
+    let expr = parse(&mut lex);
+    assert_eq!(expr, Expr::Binary {
+        op: Operator::Add,
+        lhs: Box::new(Expr::Binary {
+            op: Operator::Add,
+            lhs: Box::new(Expr::Number(1)),
+            rhs: Box::new(Expr::Number(2)),
+        }),
+        rhs: Box::new(Expr::Number(5)),
+    });
+}
+
+
+#[test]
+fn ast_test_8() {
+    let mut lex = Lexer::new_from_str("1 + 2 / 12\n");
+    let expr = parse(&mut lex);
+    assert_eq!(expr, Expr::Binary {
+        op: Operator::Add,
+        lhs: Box::new(Expr::Number(1)),
+        rhs: Box::new(Expr::Binary {
+            op: Operator::Divide,
+            lhs: Box::new(Expr::Number(2)),
+            rhs: Box::new(Expr::Number(12)),
+        }),
+    });
+}
--- compiler/src/lexing/test.rs
+++ compiler/src/lexing/test.rs
@@ -20,14 +20,20 @@ fn lexing_test_2() {
     let lex = Lexer::new_from_str(r#"
     {
        name = "Tien";
-       age = 21;
     }
-        "#);
+"#);
     let tokens: Vec<LexResult> = lex.collect();
     assert_eq!(tokens, vec![
-        Ok((1, Token::Number(1), (1,1))),
-        Ok((1, Token::Plus, (3,3))),
-        Ok((1, Token::Number(1), (5,5))),
-        Ok((1, Token::EndOfFile, (6,6)))
+        Ok((1, Token::NewLine, (1,1))),
+        Ok((2, Token::LeftBrace, (5,5))),
+        Ok((2, Token::NewLine, (6,6))),
+        Ok((3, Token::Name("name".into()), (8,11))),
+        Ok((3, Token::Equal, (13,13))),
+        Ok((3, Token::String("Tien".into()), (15,20))),
+        Ok((3, Token::Semicolon, (21,21))),
+        Ok((3, Token::NewLine, (22,22))),
+        Ok((4, Token::RightBrace, (5,5))),
+        Ok((4, Token::NewLine, (6,6))),
+        Ok((5, Token::EndOfFile, (1,1)))
     ])
 }
